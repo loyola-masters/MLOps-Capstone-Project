283e763650b39cecc3087a1670f33b67d906b9db
Bernardo Ronquillo <bernardo.ronquillo@gmail.com>
2025-04-09 10:11:56 +0200

    Package prediction into function 'predict_iris'

--- backend-iris/prediction.py
+++ backend-iris/prediction.py
@@ -14,25 +14,35 @@ model_path = hf_hub_download(repo_id="brjapon/iris-dt",
 
 model = joblib.load(model_path)
 
-
 # Example prediction (random values below)
-sample_input = pd.DataFrame([{
+sepal_length = 5.1
+sepal_width = 3.5
+petal_length = 1.4
+petal_width = 0.2
+
+def predict_iris(SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm):
+    # Convert the input parameters into a 2D list/array because
+    # scikit-learn's predict() expects a 2D array of shape (n_samples, n_features)
+    # input = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    input = pd.DataFrame([{
     "SepalLengthCm": 5.1,
     "SepalWidthCm": 3.5,
     "PetalLengthCm": 1.4,
     "PetalWidthCm": 0.2
 }])
+    prediction = model.predict(input)
+    
+    # Convert the prediction to the string label
+    if prediction == 0:
+        return 'iris-setosa'
+    elif prediction == 1:
+        return 'Iris-versicolor'
+    elif prediction == 2:
+        return 'Iris-virginica'
+    else:
+        return "Invalid prediction"
 
-prediction = model.predict(sample_input)
 
-# Convert the prediction to the string label
-if prediction == 0:
-    prediction_species = 'iris-setosa'
-elif prediction == 1:
-    prediction_species = 'Iris-versicolor'
-elif prediction == 2:
-    prediction_species = 'Iris-virginica'
-else:
-    prediction_species = "Invalid prediction"
 
-print(prediction, prediction_species)  # e.g., [0] which might correspond to 'setosa'
+prediction = predict_iris(sepal_length, sepal_width, petal_length, petal_width)
+print(prediction)  # e.g., [0] which might correspond to 'setosa'
